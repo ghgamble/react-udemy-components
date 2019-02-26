d45c5d16b5671fbe7737e4442e493c82e400dbb6
Grace Gamble <grace.h.gamble@gmail.com>
2019-02-26 12:00:32 -0700

    Props and Nesting Components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
-      return <div>Hi there!</div>;
+      return (
+            <div className="ui container comments">
+                  <ApprovalCard />
+                  <CommentDetail
+                        author="Sam"
+                        date="Yesterday at 5:15PM"
+                        text="Nice blog post!"
+                        avatar={faker.image.avatar()}
+                  />
+                  <CommentDetail
+                        author="Alex"
+                        date="Today at 5:27PM"
+                        text="Ditto!"
+                        avatar={faker.image.avatar()}
+                  />
+                  <CommentDetail
+                        author="Jane"
+                        date="Today at 7:02PM"
+                        text="Thanks Sam and Alex!"
+                        avatar={faker.image.avatar()}
+                  />
+            </div>
+      );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
